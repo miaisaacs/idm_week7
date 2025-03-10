b8c93ab9f55b63e415baa99c0b1fb46745649f94
Mia Isaacs <mbi2110@cumc.columbia.edu>
2025-03-10 15:49:01 -0400

    finishing lab report 7

--- Lab7_StochModels_forstudents.R
+++ Lab7_StochModels_forstudents.R
@@ -152,14 +152,84 @@ if(T){
 
 
 
-# [LQ3] Modify the sample code (no intervention included) to run the Ebola stochastic model with no intervention during the first 8 weeks and with intervention starting Week 9 (Day 63). 
-# For this simulation, assume when intervention is implemented, transmission rate in the community is reduced by 88% (, i.e., to 12% of the initial level), and that transmission in both the hospitals and funeral ceremonies is completed eliminated. 
-# Submit your code in your report. Also test run your code; plot and show the simulated weekly new incidence (1pt)
-# Hint: think about how to control the simulation time and what parameters you would need to use depending on whether intervention is in place. 
+# [LQ3] Modify the sample code (no intervention included) to run the Ebola stochastic model with 
+# no intervention during the first 8 weeks and with intervention starting Week 9 (Day 63). 
+# For this simulation, assume when intervention is implemented, transmission rate in the community 
+# is reduced by 88% (, i.e., to 12% of the initial level), and that transmission in both the 
+# hospitals and funeral ceremonies is completed eliminated. 
+# Submit your code in your report. Also test run your code; plot and show the simulated weekly new 
+# (1pt)
+# Hint: think about how to control the simulation time and what parameters you would need to use 
+# depending on whether intervention is in place. 
 z=.88; # intervention in the community
 z.H=1; # intervention in the hospital
 z.F=1; # intervention in safe burial
 
+while((I>0 | E>0 | H>0 | FF>0) & S>0){ # simulate epid for the first 9 week without ctrl
+  # step 1: compute the transition rates
+  if(tm<9){
+    beta.I=.588; # transmission rate in community
+    beta.H=.794; # transmission rate in hospital
+    beta.F=7.653; # transmission rate at funerals
+  } else{ #with intervention on the three transmission routes
+    beta.I=.588*(1-z);
+    beta.H=.794*(1-z.H);
+    beta.F=7.653*(1-z.F);
+    }
+  rate.se=S/N*(beta.I*I+beta.H*H+beta.F*FF);  # S->E
+  rate.ei=alpha*E;  # E->I
+  rate.ih=gamma.h*theta1*I; # I->H
+  rate.hf=gamma.dh*delta2*H;  # H->F
+  rate.fr=gamma.f*FF;  # F->R
+  rate.ir=gamma.i*(1-theta1)*(1-delta1)*I;  # I->R
+  rate.if=delta1*(1-theta1)*gamma.d*I; # I->F
+  rate.hr=gamma.ih*(1-delta2)*H; # H->R
+  rate.tot=rate.se+rate.ei+rate.ih+rate.hf+rate.fr+rate.ir+rate.if+rate.hr
+  
+  # step 2: draw a random number, u1, betw 0 and 1 and 
+  # calcualte the time after which the next transition occurs
+  u1=runif(1); # draw a random number 
+  tau_i=-log(u1)/rate.tot
+  
+  # step 3: compute the probability that each type of transition will occur based on the rates
+  # use this to calculate the range in which a number drawn at random 
+  # must lie for a given transition to occur
+  p.se=rate.se/rate.tot;  # prob the next event is S->E
+  p.ei=rate.ei/rate.tot;  # prob the next event is E->I
+  p.ih=rate.ih/rate.tot;  # prob the next event is I->H
+  p.hf=rate.hf/rate.tot;  # prob the next event is H->F
+  p.fr=rate.fr/rate.tot;  # prob the next event is F->R
+  p.ir=rate.ir/rate.tot;  # prob the next event is I->R
+  p.if=rate.if/rate.tot;  # prob the next event is I->F
+  p.hr=rate.hr/rate.tot;  # prob the next event is H->R
+  
+  # step 4: draw a random number, u2, to determine the transition event which occurs next.
+  u2=runif(1); # draw a random number 
+  
+  # step 5: use the result from step 4 to update the number of state varibles
+  if(u2<p.se) { # u2 lies in [0,p.se), the next event is S->E
+    S=S-1; E=E+1;
+  } else if (u2<p.se+p.ei) { # u2 lies in [p.se,p.se+p.ei), the next event is E->I
+    E=E-1; I=I+1;
+    cumI=cumI+1; # record cumulative incidence
+  } else if (u2<p.se+p.ei+p.ih) { # u2 lies in [p.se+p.ei,p.se+p.ei+p.ih), the next event is I->H
+    I=I-1; H=H+1;
+  } else if (u2<p.se+p.ei+p.ih+p.hf) { # u2 lies in [p.se+p.ei+p.ih,p.se+p.ei+p.ih+p.hf), the next event is H->F
+    H=H-1; FF=FF+1;
+  } else if (u2<p.se+p.ei+p.ih+p.hf+p.fr){ # FF->R
+    FF=FF-1; R=R+1;
+  } else if (u2<p.se+p.ei+p.ih+p.hf+p.fr+p.ir){ # I->R
+    I=I-1; R=R+1;
+  } else if (u2<p.se+p.ei+p.ih+p.hf+p.fr+p.ir+p.if) { # I->FF
+    I=I-1; FF=FF+1;
+  } else { # H->R
+    H=H-1; R=R+1;
+  }
+  tm=tm+tau_i; # update time
+  # save the results
+  res=rbind(res,c(tm,tau_i,S,E,I,H,FF,R,cumI))
+} 
+colnames(res)=c('time','tm_step','S','E','I','H','FF','R','cumI')
 
 if(T){
   ## EXAMPLE CODE: to aggregate the results to weekly interval
@@ -178,8 +248,6 @@ if(T){
   plot(wklyInci, type = 'l', ylab='Weekly incidence',xlab='Week')
 }
 
-
-
 ################################################################################
 ## PART 2. UNCERTAINTY TEST
 ################################################################################
@@ -197,9 +265,137 @@ z.F=1; # intervention in safe burial
 ## CODE YOURSELF TO RUN THE MODEL FOR 1000 TIMES
 ## NOTE: IT WILL TAKE A WHILE (WAIT... WAIT... WAIT...) TO RUN
 
+num_run=1000;
+out=matrix(0,num_run,4); colnames(out)=c('num_run','duration','peak','final.size');
+out[,1]=1:num_run;
+for(ir in 1:num_run){
+  N=2e5; E0=H0=FF0=R0=0; I0=3; S0=N-I0; 
+  tm=0; # in week
+  S=S0; E=E0; I=I0; H=H0; FF=FF0; R=R0; cumI=I0;
+  res=c(tm,S,E,I,H,FF,R,cumI);
+  while((I>0 | E>0 | H>0 | FF>0) & S>0){ # simulate epid for the first 9 week without ctrl
+    # step 1: compute the transition rates
+    if(tm<9){
+      beta.I=.588; # transmission rate in community
+      beta.H=.794; # transmission rate in hospital
+      beta.F=7.653; # transmission rate at funerals
+    } else{ #with intervention on the three transmission routes
+      beta.I=.588*(1-z);
+      beta.H=.794*(1-z.H);
+      beta.F=7.653*(1-z.F);
+    }
+    rate.se=S/N*(beta.I*I+beta.H*H+beta.F*FF);  # S->E
+    rate.ei=alpha*E;  # E->I
+    rate.ih=gamma.h*theta1*I; # I->H
+    rate.hf=gamma.dh*delta2*H;  # H->F
+    rate.fr=gamma.f*FF;  # F->R
+    rate.ir=gamma.i*(1-theta1)*(1-delta1)*I;  # I->R
+    rate.if=delta1*(1-theta1)*gamma.d*I; # I->F
+    rate.hr=gamma.ih*(1-delta2)*H; # H->R
+    rate.tot=rate.se+rate.ei+rate.ih+rate.hf+rate.fr+rate.ir+rate.if+rate.hr
+    
+    # step 2: draw a random number, u1, betw 0 and 1 and 
+    # calcualte the time after which the next transition occurs
+    u1=runif(1); # draw a random number 
+    tau_i=-log(u1)/rate.tot
+    
+    # step 3: compute the probability that each type of transition will occur based on the rates
+    # use this to calculate the range in which a number drawn at random 
+    # must lie for a given transition to occur
+    p.se=rate.se/rate.tot;  # prob the next event is S->E
+    p.ei=rate.ei/rate.tot;  # prob the next event is E->I
+    p.ih=rate.ih/rate.tot;  # prob the next event is I->H
+    p.hf=rate.hf/rate.tot;  # prob the next event is H->F
+    p.fr=rate.fr/rate.tot;  # prob the next event is F->R
+    p.ir=rate.ir/rate.tot;  # prob the next event is I->R
+    p.if=rate.if/rate.tot;  # prob the next event is I->F
+    p.hr=rate.hr/rate.tot;  # prob the next event is H->R
+    
+    # step 4: draw a random number, u2, to determine the transition event which occurs next.
+    u2=runif(1); # draw a random number 
+    
+    # step 5: use the result from step 4 to update the number of state varibles
+    if(u2<p.se) { # u2 lies in [0,p.se), the next event is S->E
+      S=S-1; E=E+1;
+    } else if (u2<p.se+p.ei) { # u2 lies in [p.se,p.se+p.ei), the next event is E->I
+      E=E-1; I=I+1;
+      cumI=cumI+1; # record cumulative incidence
+    } else if (u2<p.se+p.ei+p.ih) { # u2 lies in [p.se+p.ei,p.se+p.ei+p.ih), the next event is I->H
+      I=I-1; H=H+1;
+    } else if (u2<p.se+p.ei+p.ih+p.hf) { # u2 lies in [p.se+p.ei+p.ih,p.se+p.ei+p.ih+p.hf), the next event is H->F
+      H=H-1; FF=FF+1;
+    } else if (u2<p.se+p.ei+p.ih+p.hf+p.fr){ # FF->R
+      FF=FF-1; R=R+1;
+    } else if (u2<p.se+p.ei+p.ih+p.hf+p.fr+p.ir){ # I->R
+      I=I-1; R=R+1;
+    } else if (u2<p.se+p.ei+p.ih+p.hf+p.fr+p.ir+p.if) { # I->FF
+      I=I-1; FF=FF+1;
+    } else { # H->R
+      H=H-1; R=R+1;
+    }
+    tm=tm+tau_i; # update time
+    # save the results
+    res=rbind(res,c(tm,S,E,I,H,FF,R,cumI))
+  } 
+  colnames(res)=c('time','S','E','I','H','FF','R','cumI')
+  ## compile results to weekly interval
+  num_wk=ceiling(res[nrow(res),'time']) # epidemic duration
+  wkly.res=matrix(NA,num_wk,8); colnames(wkly.res)=c('time','S','E','I','H','FF','R','cumI')
+  num_events=NULL;
+  for(wk in 1:num_wk){
+    idx=tail(which(res[,'time']<wk),1);
+    # idx also records the number of events happened from time 0 to current week
+    num_events=c(num_events,ifelse(wk==1,idx,idx-sum(num_events)));
+    wkly.res[wk,]=res[idx,];
+  }
+  wklyInci=wkly.res[-1,'cumI']-wkly.res[-nrow(wkly.res),'cumI']
+  
+  ## save result for this run
+  out[ir,'duration']=num_wk;
+  out[ir,'peak']=max(wklyInci);
+  out[ir,'final.size']=wkly.res[nrow(wkly.res),'cumI'];
+  print(c(paste('run #',ir),out[ir,2:4]));
+}
 
 
 
 ## PLOT THE DISTRIBUTION
 ## HINT: USE THE FUNCITON: hist() to plot histogram
+
+# Assuming 'out' is your results matrix with columns: num_run, duration, peak, final.size
+
+# Extracting the columns for easier reference
+duration_data <- out[, 'duration']
+peak_data <- out[, 'peak']
+final_size_data <- out[, 'final.size']
+
+# Setting up the plotting area to accommodate multiple plots
+par(mfrow = c(1, 3))  # Adjust the layout as needed
+
+# Histogram for 'duration'
+hist(duration_data, 
+     main = 'Distribution of Epidemic Duration',
+     xlab = 'Duration (weeks)',
+     col = 'lightblue',
+     border = 'black',
+     breaks = 20)  # Adjust the number of bins as needed
+
+# Histogram for 'peak'
+hist(peak_data, 
+     main = 'Distribution of Peak Incidence',
+     xlab = 'Peak Incidence',
+     col = 'lightgreen',
+     border = 'black',
+     breaks = 20)  # Adjust the number of bins as needed
+
+# Histogram for 'final.size'
+hist(final_size_data, 
+     main = 'Distribution of Final Size',
+     xlab = 'Final Size',
+     col = 'lightcoral',
+     border = 'black',
+     breaks = 20)  # Adjust the number of bins as needed
+
+# Resetting the plotting area to a single plot
+par(mfrow = c(1, 1))
 
